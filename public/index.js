e0d03f448be3118604870c5fde07be6e8d01003f
Jeremy Brown <jeremy@geistinteractive.com>
2021-05-30 18:01:20 -0400

    tooke out wrong code

--- public/index.js
+++ public/index.js
@@ -1,12 +1,10 @@
-var jsonata = require("jsonata");
-
 const returnToFM = (result) => {
   const obj = Array.isArray(result)
     ? JSON.stringify(result)
     : typeof result === "object"
     ? JSON.stringify(result)
     : result;
-  FileMaker.PerformScriptWithOption("Get Result", obj, 5);
+  FileMaker.PerformScript("Get Result", obj, 5);
 };
 
 window.simpleQuery = function (json, query) {
