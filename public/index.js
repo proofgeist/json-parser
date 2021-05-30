f8828721de0e5b48f40bc1728e8fd89856d96236
Jeremy Brown <jeremy@geistinteractive.com>
2021-05-30 16:42:16 -0400

    first commit, setting up project

---
+++ public/index.js
@@ -0,0 +1,18 @@
+var jsonata = require("jsonata");
+
+const returnToFM = (result) => {
+  const obj = Array.isArray(result)
+    ? JSON.stringify(result)
+    : typeof result === "object"
+    ? JSON.stringify(result)
+    : result;
+  FileMaker.PerformScriptWithOption("Get Result", obj, 5);
+};
+
+window.simpleQuery = function (json, query) {
+  // alert("Simple")
+  var j = JSON.parse(json);
+  console.log(j);
+  var result = jsonata(query).evaluate(j);
+  returnToFM(result);
+};
