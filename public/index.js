2d826348371b75721e2f8a8b26589dc2ed8c396f
Jeremy Brown <jeremy@geistinteractive.com>
2021-06-28 08:43:55 -0400

    all changes

--- public/index.js
+++ public/index.js
@@ -1,16 +1,29 @@
 const returnToFM = (result) => {
-  const obj = Array.isArray(result)
-    ? JSON.stringify(result)
-    : typeof result === "object"
-    ? JSON.stringify(result)
-    : result;
-  FileMaker.PerformScript("Get Result", obj, 5);
+  const obj =
+    Array.isArray(result) || typeof result === "object"
+      ? JSON.stringify(result)
+      : result;
+  FileMaker.PerformScript("Get Result", obj);
+};
+const returnToFMInScript = (result) => {
+  const obj =
+    Array.isArray(result) || typeof result === "object"
+      ? JSON.stringify(result)
+      : result;
+  FileMaker.PerformScriptWithOption("Get Result In Script", obj, 5);
 };
 
 window.simpleQuery = function (json, query) {
-  // alert("Simple")
+  // alert("Simple");
   var j = JSON.parse(json);
   console.log(j);
   var result = jsonata(query).evaluate(j);
   returnToFM(result);
+};
+window.simpleQueryInScript = function (json, query) {
+  // alert("Simple");
+  var j = JSON.parse(json);
+  console.log(j);
+  var result = jsonata(query).evaluate(j);
+  returnToFMInScript(result);
 };
