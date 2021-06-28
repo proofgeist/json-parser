2e8cdc02226eb5ddb70333ade1ded817319a84c8
Jeremy Brown <jeremy@geistinteractive.com>
2021-06-28 13:52:43 -0400

    updated code

--- public/index.js
+++ public/index.js
@@ -1,27 +1,22 @@
+import { callFMScript } from "fm-webviewer-fetch";
+
 const returnToFM = (result) => {
-  const obj =
-    Array.isArray(result) || typeof result === "object"
-      ? JSON.stringify(result)
-      : result;
-  FileMaker.PerformScript("Get Result", obj);
+  const obj = { result };
+
+  callFMScript("Get Result", obj);
 };
 const returnToFMInScript = (result) => {
-  const obj =
-    Array.isArray(result) || typeof result === "object"
-      ? JSON.stringify(result)
-      : result;
+  const obj = JSON.stringify({ result });
   FileMaker.PerformScriptWithOption("Get Result In Script", obj, 5);
 };
 
-window.simpleQuery = function (json, query) {
-  // alert("Simple");
+window.simpleQuery = function (json, query, option = null) {
   var j = JSON.parse(json);
   console.log(j);
   var result = jsonata(query).evaluate(j);
   returnToFM(result);
 };
 window.simpleQueryInScript = function (json, query) {
-  // alert("Simple");
   var j = JSON.parse(json);
   console.log(j);
   var result = jsonata(query).evaluate(j);
